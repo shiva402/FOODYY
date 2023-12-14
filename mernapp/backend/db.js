56e4f31d8fe9a35a560fe16d103534cafc609622
shiva17 <shivasainimmathi17@gmail.com>
2023-12-14 13:10:00 +0530

    first commit

---
+++ mernapp/backend/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://foodyy:shiva7777777@cluster0.qfaysv8.mongodb.net/foodyymern?retryWrites=true&w=majority'
+const mongoDB =async() =>{
+   await mongoose.connect(mongoURI,{ useNewUrlParser: true },async(err,result)=>{
+    if(err) console.log("---",err)
+    else{
+        console.log("connected");
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray(async function( err, data){
+            const foodCategory = await mongoose.connection.db.collection("foodCategory");
+            foodCategory.find({}).toArray(function (err,catData){
+                if(err) console.log(err);
+                else{
+                     global.food_items = data;
+                     global.foodCategory = catData;
+                }
+            })
+           // if(err) console.log(err);
+           // else{
+           //    global.food_items = data;
+           //     
+           // }
+        })
+    }    
+
+    });
+}    
+
+
+module.exports = mongoDB;
